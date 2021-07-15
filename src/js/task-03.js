7b1472a675db92b8ec79918097b363bb2d811fd2
SerheiYakimov <ysergey2901@gmail.com>
2021-07-16 02:14:45 +0300

    решаю 3 задание

---
+++ src/js/task-03.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// Используй массив объектов images для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
+
+
+
+const images = [
+    {
+      url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'White and Black Long Fur Cat',
+    },
+    {
+      url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+      url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Group of Horses Running',
+    },
+  ];
+
+  const galleryItemEl = document.createElement('li');
+
+  const image = document.createElement('img');
+  image.setAttributes('url', `${images.url.value}`);
+  image.setAttributes('alt', `${images.alt.value}`);
+
+  
+//   galleryItemEl.insertAdjacentHTML('afterbegin', `https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`);
+
+
+  const galleryEl = document.querySelector('#gallery');
+  galleryEl.append(galleryItemEl);
