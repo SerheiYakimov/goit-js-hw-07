16c97bb96d12750b7a41f721596e9a8ff9c5fbb5
SerheiYakimov <ysergey2901@gmail.com>
2021-07-20 18:57:14 +0300

    внес правки во 2 задачу

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -12,28 +12,40 @@ const ingredients = [
     'Помидоры',
     'Зелень',
     'Приправы',
-  ];
+];
+  
+console.log(ingredients);
+ 
+const ingredientsList = document.querySelector('#ingredients');
 
+const ingredientsListEl = ingredients.map(element => {
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.textContent = element.value;
+});
   
-  const potato = document.createElement('li');
-  potato.textContent = 'Картошка';
   
-  const mushrooms = document.createElement('li');
-  mushrooms.textContent = 'Грибы';
+    
   
-  const garlic = document.createElement('li');
-  garlic.textContent = 'Чеснок';
   
-  const tomato = document.createElement('li');
-  tomato.textContent = 'Помидоры';
+  // const potato = document.createElement('li');
+  // potato.textContent = 'Картошка';
   
-  const greens = document.createElement('li');
-  greens.textContent = 'Зелень';
+  // const mushrooms = document.createElement('li');
+  // mushrooms.textContent = 'Грибы';
   
-  const condiments = document.createElement('li');
-  condiments.textContent = 'Приправы';
+  // const garlic = document.createElement('li');
+  // garlic.textContent = 'Чеснок';
   
-  const ingredientsEl = document.querySelector('#ingredients');
-  ingredientsEl.append(potato, mushrooms, garlic, greens, tomato, greens, condiments);
+  // const tomato = document.createElement('li');
+  // tomato.textContent = 'Помидоры';
   
-  console.log(ingredientsEl);
+  // const greens = document.createElement('li');
+  // greens.textContent = 'Зелень';
+  
+  // const condiments = document.createElement('li');
+  // condiments.textContent = 'Приправы';
+  
+ 
+ingredientsList.append(...ingredientsListEl);
+  
+  console.log(ingredientsList);
